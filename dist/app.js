f6c429a3c124bc7044909f195af31c4e7f785b7a
= <sagarsharmatech00@gmail.com>
2024-08-07 13:34:59 +0530

    graphQL learning end for this repo

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 import { connectDB } from "./database/database.js";
 import { connectGraphQl } from "./graphQl/graphql.js";
-import express from "express";
 import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 dotenv.config({ path: "./.env" });
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 const Port = Number(process.env.port) || 3000;
@@ -15,6 +17,15 @@ const app = express();
 app.use(express.json({
     limit: "10MB"
 }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*", credentials: true }));
+app.use(morgan("dev"));
+app.get("*", (req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: "Page Not Found"
+    });
+});
 app.get("/", (req, res) => {
     res.send("Home");
 });
@@ -22,3 +33,6 @@ app.use("/graphql", expressMiddleware(graphQLServer));
 app.listen(Port, () => {
     console.log(`app is listening on port: ${Port}`);
 });
+// function cors(arg0: { origin: string; credential: boolean; }): any {
+//   throw new Error("Function not implemented.");
+// }
